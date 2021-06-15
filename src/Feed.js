c3e649bdb383923cfa7993e5a2b77f43c25d49c7
IronNash <iron.nash@gmail.com>
2021-06-15 01:28:50 -0300

    Call it a day

---
+++ src/Feed.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Feed.css";
+import Post from "./Post";
+
+function Feed() {
+  return (
+    <div className="feed">
+      <Post />
+    </div>
+  );
+}
+
+export default Feed;
---
+++ src/Header.js
@@ -0,0 +1,57 @@
+import React from "react";
+import "./Header.css";
+import logo from "./logo.png";
+import SearchIcon from "@material-ui/icons/Search";
+import HomeIcon from "@material-ui/icons/Home";
+import SubscriptionsOutlinedIcon from "@material-ui/icons/SubscriptionsOutlined";
+import RssFeedOutlinedIcon from "@material-ui/icons/RssFeedOutlined";
+import { Avatar, IconButton } from "@material-ui/core";
+import AddIcon from "@material-ui/icons/Add";
+import ForumIcon from "@material-ui/icons/Forum";
+import SettingsIcon from "@material-ui/icons/Settings";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <img src={logo} alt="logo" />
+        <div className="header__input">
+          <SearchIcon />
+          <input placeholder="Search Ourspace" />
+        </div>
+      </div>
+      <div className="header__middle">
+        <div className="header__option">
+          <HomeIcon fontSize="large" />
+        </div>
+        <div className="header__option">
+          <SubscriptionsOutlinedIcon fontSize="large" />
+        </div>
+        <div className="header__option">
+          <RssFeedOutlinedIcon fontSize="large" />
+        </div>
+      </div>
+      <div className="header__right">
+        <div className="header__info">
+          <Avatar />
+          <h4>Charlie Nash</h4>
+        </div>
+        <IconButton>
+          <AddIcon />
+        </IconButton>
+        <IconButton>
+          <ForumIcon />
+        </IconButton>
+        <IconButton>
+          <SettingsIcon />
+        </IconButton>
+        <IconButton>
+          <ExpandMoreIcon />
+        </IconButton>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
