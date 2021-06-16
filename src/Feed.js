2292f7ee2224cca0f0efa37b80195c29934fc964
IronNash <iron.nash@gmail.com>
2021-06-15 22:10:13 -0300

    Como diria willian bonner

--- src/Feed.js
+++ src/Feed.js
@@ -1,11 +1,21 @@
 import React from "react";
 import "./Feed.css";
 import Post from "./Post";
+import Wall from "./Wall";
 
 function Feed() {
   return (
     <div className="feed">
       <Post />
+      <Wall
+        profilePic="https://i.ytimg.com/vi/xxyJsDKTyHw/maxresdefault.jpg"
+        message="vai da bom"
+        timestamp="hora H"
+        username="sua mãe"
+        image="https://i.ytimg.com/vi/xxyJsDKTyHw/maxresdefault.jpg"
+      />
+      <Wall />
+      <Wall />
     </div>
   );
 }
--- src/Post.js
+++ src/Post.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Post.css";
 import { Avatar } from "@material-ui/core";
+import VideocamIcon from "@material-ui/icons/Videocam";
+import PhotoLibraryIcon from "@material-ui/icons/PhotoLibrary";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
 
 function Post() {
+  const [input, setInput] = useState("");
+  const [imageUrl, setiImageUrl] = useState("");
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    // reseting state after submit
+    setInput("");
+    setiImageUrl("");
   };
 
   return (
@@ -13,16 +22,35 @@ function Post() {
         <Avatar />
         <form>
           <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
             className="post__input"
             placeholder="A small post for mankind"
           />
-          <input placeholder="image URL (Optional)" />
+          <input
+            value={imageUrl}
+            onChange={(e) => setiImageUrl(e.target.value)}
+            placeholder="image URL (Optional)"
+          />
           <button onClick={handleSubmit} type="submit">
             Submit
           </button>
         </form>
       </div>
-      <div className="post__bottom"></div>
+      <div className="post__bottom">
+        <div className="post__option">
+          <VideocamIcon style={{ color: "red" }} />
+          <h3>Live VIdeo</h3>
+        </div>
+        <div className="post__option">
+          <PhotoLibraryIcon style={{ color: "green" }} />
+          <h3>Photo/Video</h3>
+        </div>
+        <div className="post__option">
+          <InsertEmoticonIcon style={{ color: "orange" }} />
+          <h3>Feeling/Status</h3>
+        </div>
+      </div>
     </div>
   );
 }
